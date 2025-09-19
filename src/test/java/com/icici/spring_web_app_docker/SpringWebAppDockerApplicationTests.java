1224a14527839b827f1d802940c1988386f3086b
Puneet Vashisht <puneetvashisht@gmail.com>
2025-09-19 14:28:54 +0530

    final commit

--- src/test/java/com/icici/spring_web_app_docker/SpringWebAppDockerApplicationTests.java
+++ src/test/java/com/icici/spring_web_app_docker/SpringWebAppDockerApplicationTests.java
@@ -1,7 +1,6 @@
 package com.icici.spring_web_app_docker;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
