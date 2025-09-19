18df04c7836142d4e0777a6fbcdf2c663b276e8b
Puneet Vashisht <puneetvashisht@gmail.com>
2025-09-19 14:09:01 +0530

    6

--- src/test/java/com/icici/spring_web_app_docker/SpringWebAppDockerApplicationTests.java
+++ src/test/java/com/icici/spring_web_app_docker/SpringWebAppDockerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.icici.spring_web_app_docker;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringWebAppDockerApplicationTests {
 
+	@Autowired
+    private MockMvc mockMvc;
+
+
+	// write test case for controller class
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() throws Exception {
+	    mockMvc.perform(get("/hello"))
+	           .andExpect(status().isOk())
+	           .andExpect(content().string("Hello, World!"));
 	}
+	
+		
 
 }
